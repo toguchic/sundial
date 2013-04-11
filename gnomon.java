8cb3f21abd1a7eafc8fae070826c7cde3f21da73
Matt Kanda <mtkanda@hawaii.edu>
2013-04-11 10:52:15 -1000

    Added method to convert hour and min to arc

--- gnomon.java
+++ gnomon.java
@@ -11,34 +11,44 @@ public class gnomon {
 		 * d -> angle which the hour line makes with the gnomon
 		 * t -> time measured from noon in degrees of arc
 		 * phi -> latitude of the place the sundial is located
+		 * 
+		 * 12 noon is 180* of arc
 		 */
 		
-		double d = 0;
-		double t = 9;
-		double phi = 11;
+		//double d = 0;
+		//double t = 9;
+		//double phi = 11;
 		
-		d = Math.tan(t) * Math.sin(phi);
-		d = Math.tan(d);
+		//d = Math.tan(t) * Math.sin(phi);
+		//d = Math.tan(d);
 		
-		System.out.println(d);
+		//System.out.println(d);
+		
+		System.out.println(calculateArc(0,4));
 	}
 	
-	public double hourToDegree(double h){
+	private double calculateArc(int h, int m){
+		int convertToMin = h * 60;
+		m = m + convertToMin;
+		return minToDegree(m);
+	}
+	
+	private double hourToDegree(double h){
 		double htd = h * 15;
 		return htd;
 	}
 	
-	public double minToDegree(double m){
+	private double minToDegree(double m){
 		double mtd = m / 4;
 		return mtd;
 	}
 	
-	public double minToFt(double m){
+	private double minToFt(double m){
 		double mtf = m * 15;
 		return mtf;
 	}
 	
-	public double secToFt(double s){
+	private double secToFt(double s){
 		double stf = s / 4;
 		return stf;
 	}
