6ff688af14dde1897cdd8740d0cefbcdce31378a
samueldj <samueldj@hawaii.edu>
2013-04-17 20:05:42 -1000

    Update EOT.java

--- EOT.java
+++ EOT.java
@@ -3,10 +3,10 @@ import java.util.Calendar;
 public class EOT {
     private Calendar calendarDate;
   public double EqOfT(){
-        int DOY = calandarDate.get(Calendar.DAY_OF_YEAR);
-        double B = ((DOY-81)/365)*360;
+        int DOY = caledarDate.get(Calendar.DAY_OF_YEAR);
+        double B = ((DOY - 81) / 365) * 360;
         B = Math.toRadians(B);
-        double E = 9.87*Math.sin(2*B) - 7.53*Math.cos(B) - 1.5*Math.sin(B);
+        double E = 9.87 * Math.sin(2 * B) - 7.53 * Math.cos(B) - 1.5 * Math.sin(B);
         return E;
     }
 }
