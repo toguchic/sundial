f43818a0a5e292c56b3723d651e3bc56b2e9410a
samueldj <samueldj@hawaii.edu>
2013-04-17 18:39:43 -1000

    Created EOT

---
+++ EOT.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+
+public class EOT {
+    private Calendar date;
+  public double EqOfT(){
+        int DOY = date.get(Calendar.DAY_OF_YEAR);
+        double B = ((DOY-81)/365)*360;
+        B = Math.toRadians(B);
+        double E = 9.87*Math.sin(2*B) - 7.53*Math.cos(B) - 1.5*Math.sin(B);
+        return E;
+    }
+}
