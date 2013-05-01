5a13e6dc5d9f471ebfed585c6a7fcd463878ef7a
toguchic <toguchic@hawaii.edu>
2013-05-01 00:03:13 -1000

    Update EOT.java

--- EOT.java
+++ EOT.java
@@ -1,25 +1,39 @@
-import java.util.*;
+/**************************
+ * Calculates Equaton of Time
+ * 
+ * @ Authors: Chriselle TOguchi, Matt Kanda, Samuel Kim
+ * @ Date: 4/30/2013
+ * @ Version: 1.0
+ * 
+ * 
+ * *****************************/
+ 
+ import java.util.*;
 
 public class EOT {
-
-    public static double EqOfT(int month, int day, int year) {
+    
+    
+    	//Calculates the Equation of Time
+	public static double EqOfT(int month, int day, int year) {
 		double DOY = dayOfYear(month,day,year);
-        double B = 360 * (DOY-81) / 365;
-        B = Math.toRadians(B);
-        double E = 9.87 * Math.sin(2*B) - 7.53*Math.cos(B) - 1.5*Math.sin(B);
-        return E;
-    }
-   	
+        	double B = 360 * (DOY-81) / 365;
+       		B = Math.toRadians(B);
+        	double E = 9.87 * Math.sin(2*B) - 7.53*Math.cos(B) - 1.5*Math.sin(B);
+        	return E;
+    	}
+   	//Calculates the Day of the Year
 	public static int dayOfYear(double month, double day, double year) {
 	    short[] daysInMonth = new short[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	    short[] daysInMonthLeap = new short[]{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	    boolean isLeapYear = (year % 4 == 0 && year % 100 != 0)||(year % 400 == 0);
 	    int yearDay = 0;
+	    
 	    for(int i=0; i<month-1; i++)
 	    {
 	        if(isLeapYear) yearDay += daysInMonthLeap[i];
 	        else yearDay += daysInMonth[i];
 	    }
+	    
 	    yearDay+=day;
 	    return yearDay;
 	}
